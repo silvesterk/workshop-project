aca0c4b981a5071f7703cad5602dd9dc3c9c941d
Norbert Specht <nosp@SAG-C02VH4XQHTD5.local>
2021-06-03 10:57:50 +0200

    adding radii to avatar component

--- design-system/src/components/Avatar/index.js
+++ design-system/src/components/Avatar/index.js
@@ -1,6 +1,7 @@
 import { string } from "prop-types";
 import React from "react";
 import styled from "styled-components";
+import { radii16RadiiTopLeft  } from '../../tokens/dist/radii';
 
 /* Avatar
  *
@@ -11,7 +12,7 @@ const Avatar = ({ src, ...props }) => <StyledAvatar src={src} {...props} />;
 const StyledAvatar = styled.img`
   width: 48px;
   height: 48px;
-  border-radius: 16px;
+  border-radius: ${radii16RadiiTopLeft };
 `;
 
 Avatar.propTypes = {
