bad5fe485c0f0d7c8ae4afca18d6c63ad99c0d89
Silvester Kokai <silvester.kokai@infostud.com>
2021-06-03 10:46:54 +0200

    Storybook url change

--- design-system/.creevey/config.js
+++ design-system/.creevey/config.js
@@ -3,6 +3,5 @@ const path = require('path');
 module.exports = {
   screenDir: path.join(__dirname, '../tests/referenceImages'),
   reportDir: path.join(__dirname, '../tests/report'),
-  storybookUrl: 'http://localhost:6006',
   diffOptions: { threshold: 0.2 },
 }
