b4d4abadc993943dd1e928eaac7b3f9d0bd12f4c
Silvester Kokai <silvester.kokai@infostud.com>
2021-06-03 10:46:00 +0200

    Export radii from index.js

--- design-system/.creevey/config.js
+++ design-system/.creevey/config.js
@@ -3,5 +3,6 @@ const path = require('path');
 module.exports = {
   screenDir: path.join(__dirname, '../tests/referenceImages'),
   reportDir: path.join(__dirname, '../tests/report'),
+  storybookUrl: 'http://localhost:6006',
   diffOptions: { threshold: 0.2 },
 }
